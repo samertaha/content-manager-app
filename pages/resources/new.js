1b5d924e9f42af454260e8096011bbaa243dfc65
Samer taha <alrazy1@gmail.com>
2025-01-21 14:28:11 +0200

    added submit new resource post call to the express endpoint

--- pages/resources/new.js
+++ pages/resources/new.js
@@ -1,6 +1,32 @@
 import Layout from "@/components/Layout";
+import { useState } from "react";
+
+const DEFAULT_DATA = {
+  title: "",
+  description: "",
+  link: "",
+  priority: "2",
+  timeToFinish: 60,
+};
 
 const ResourceCreate = () => {
+  const [form, setForm] = useState(DEFAULT_DATA);
+
+  const submitForm = () => {
+    fetch("/api/resources", {
+      body: JSON.stringify(form),
+      headers: { "Content-Type": "application/json" },
+      method: "POST",
+    });
+  };
+
+  const resetForm = () => setForm(DEFAULT_DATA);
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setForm({ ...form, [name]: value });
+  };
+
   return (
     <Layout>
       <div className="container">
@@ -13,6 +39,9 @@ const ResourceCreate = () => {
                   <label className="label">Title</label>
                   <div className="control">
                     <input
+                      value={form.title}
+                      onChange={handleChange}
+                      name="title"
                       className="input"
                       type="text"
                       placeholder="Learn Next JS and Sanity IO"
@@ -23,6 +52,9 @@ const ResourceCreate = () => {
                   <label className="label">Description</label>
                   <div className="control">
                     <textarea
+                      value={form.description}
+                      name="description"
+                      onChange={handleChange}
                       className="textarea"
                       placeholder="Learn these technologies because they are very popular and enable better SEO"
                     ></textarea>
@@ -32,6 +64,9 @@ const ResourceCreate = () => {
                   <label className="label">Link</label>
                   <div className="control">
                     <input
+                      value={form.link}
+                      onChange={handleChange}
+                      name="link"
                       className="input"
                       type="text"
                       placeholder="https://academy.eincode.com"
@@ -42,7 +77,11 @@ const ResourceCreate = () => {
                   <label className="label">Priority</label>
                   <div className="control">
                     <div className="select">
-                      <select>
+                      <select
+                        value={form.priority}
+                        onChange={handleChange}
+                        name="priority"
+                      >
                         <option>1</option>
                         <option>2</option>
                         <option>3</option>
@@ -54,18 +93,34 @@ const ResourceCreate = () => {
                   <label className="label">Time to finnish</label>
                   <div className="control">
                     <input
+                      value={form.timeToFinish}
+                      onChange={handleChange}
+                      name="timeToFinish"
                       className="input"
                       type="number"
-                      placeholder="60 (time is in minutes)"
+                      placeholder="60"
                     />
                   </div>
+                  <p className="help">time is in minutes</p>
                 </div>
                 <div className="field is-grouped">
                   <div className="control">
-                    <button className="button is-link">Submit</button>
+                    <button
+                      type="button"
+                      onClick={submitForm}
+                      className="button is-link"
+                    >
+                      Submit
+                    </button>
                   </div>
                   <div className="control">
-                    <button className="button is-link is-light">Cancel</button>
+                    <button
+                      onClick={resetForm}
+                      type="button"
+                      className="button is-link is-light"
+                    >
+                      Reset Form
+                    </button>
                   </div>
                 </div>
               </form>
